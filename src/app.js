376a8e5fe03a7661c6f712262934af90e63cad2c
Ricardo0331 <ricardose0419@gmail.com>
2023-10-15 18:16:06 -0500

    Added /notes route that will handle a post

--- src/app.js
+++ src/app.js
@@ -3,20 +3,31 @@ const app = express();
 
 const notes = require("./data/notes-data");
 
-app.get("/notes/:noteId", (req, res) => {
-  const noteId = Number(req.params.noteId);
-  const foundNote = notes.find((note) => note.id === noteId);
-  res.json({ data: foundNote });
+app.post("/notes", (req, res) => {
+  //validate incoming request
+  const { data = {} } = req.body;
+  const { text } = data;
+
+  if (!text) {
+    return res.status(400).send("The text property is missing or empty");
+  }
+
+  //generate new ID for the note
+  const newId = Math.max(...notes.map((note) => note.id), 0) + 1;
+
+  //create new note
+  const newNote = {
+    id: newId,
+    text,
+  };
+
+  //store new note
+  notes.push(newNote);
+
+  //return the new note with a 201 status code
+  res.status(201).json({ data : newNote});
 });
 
-app.get("/notes", (req, res) => {
-  res.json({ data: notes });
-});
 
-// TODO: Add ability to create a new note
-
-// TODO: Add not-found handler
-
-// TODO: Add error handler
 
 module.exports = app;
